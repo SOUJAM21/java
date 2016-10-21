99c19eae902ebaf37350409afcb6f67ece506ec6
Sourabh <sourabh.jamalapuram@gmail.com>
2016-10-20 22:17:52 -0700

    Classes,Objects,& Methods

--- Udemy/Video17/src/Application.java
+++ Udemy/Video17/src/Application.java
@@ -1,6 +1,16 @@
 class Person{
 	String name;
 	int age;
+	
+	void speak(){
+		for(int i=0; i<3; i++){
+			System.out.println("My name is: " + name + "and i am" + age + "years old");
+		}
+	}
+	
+	void sayHello(){
+		System.out.println("Hello there!");
+	}
 }
 
 
@@ -9,10 +19,14 @@ public static void main(String[] args) {
 	Person person1 = new Person();
 	person1.name = "Sourabh Jamalapuram";
 	person1.age = 37;
+	person1.speak();
+	person1.sayHello();
 	
 	Person person2 = new Person();
 	person2.name = "Sai Jamalapuram";
 	person2.age = 20;
+	person2.speak();
+	person2.sayHello();
 	
 	System.out.println(person1.name + person1.age);
 }
