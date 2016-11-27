9a4f04c3ddfde379496d60444662732e988f68d7
Sourabh <sourabh.jamalapuram@gmail.com>
2016-11-26 16:32:54 -0800

    View,Find,Quit

---
+++ Practice Assignments/PersonManager/src/Menu.java
@@ -0,0 +1,9 @@
+
+public class Menu {
+	public void showOptions(){
+		System.out.println("Choose Option");
+		System.out.println("1:View");
+		System.out.println("2:Find");
+		System.out.println("3:Quit");
+	}
+}
