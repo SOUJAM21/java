f098ad2f22ea827cbfc625a7504fa8e48b674cfc
Sourabh <sourabh.jamalapuram@gmail.com>
2017-01-11 12:37:58 -0800

    Format Fixes

--- Practice Assignments/EvenCountDown/src/Application.java
+++ Practice Assignments/EvenCountDown/src/Application.java
@@ -2,20 +2,20 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Application {
-public static void main(String[] args) {
-	System.out.println("Please Enter a Number:");
-	Scanner s1 = new Scanner(System.in);
-	int userInput = s1.nextInt();
-	
-	Sampler sample = new Sampler();
-	
-	ArrayList<Integer> countDown = sample.getEvenNumbers(userInput);
-	
-	for(int x=0 ; x<countDown.size(); x++){
-		int number = (int) countDown.get(x);
-		System.out.printf("%d %d\n",number, number*2);
+	public static void main(String[] args) {
+		System.out.println("Please Enter a Number:");
+		Scanner s1 = new Scanner(System.in);
+		int userInput = s1.nextInt();
+
+		Sampler sample = new Sampler();
+
+		ArrayList<Integer> countDown = sample.getEvenNumbers(userInput);
+
+		for (int x = 0; x < countDown.size(); x++) {
+			int number = (int) countDown.get(x);
+			System.out.printf("%d %d\n", number, number * 2);
+		}
+
+		// System.out.println(countDown);
 	}
-	
-//	System.out.println(countDown);
-}
 }
