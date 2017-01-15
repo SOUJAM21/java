aca6a623e266ecde1aff91ae8bcc00db9bf27f8d
Sourabh <sourabh.jamalapuram@gmail.com>
2017-01-14 16:07:57 -0800

    git info with branches info added

---
+++ Practice Assignments/CheckPassFail/src/Application.java
@@ -0,0 +1,16 @@
+/*
+ * Trying if-else statement.
+ */
+public class Application {  // Save as "CheckPassFail.java"
+   public static void main(String[] args) {  // Program entry point
+      int mark = 49;  // Set the value of "mark" here!
+      System.out.println("The mark is " + mark);
+ 
+      if (mark>=50 ) {
+         System.out.println("Pass");
+      } else {
+         System.out.println("Fail");
+      }
+      System.out.println("Done");
+   }
+}
