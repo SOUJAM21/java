dbff10892390b39e7a6ebff73a594f09b2911423
Sourabh <sourabh.jamalapuram@gmail.com>
2017-05-16 21:45:57 -0700

    show,created objects, assignments

--- Visual Studio Code/Tower of Hanoi/Game.java
+++ Visual Studio Code/Tower of Hanoi/Game.java
@@ -1,8 +1,13 @@
+import java.util.ArrayList;
+
 public class Game{
+    ArrayList<Tower> towers = new ArrayList<Tower>();
     public void play(){
         //setup
+        setUp();
     //loop
         //show
+        show();
         //move
         //if(isGameDone){
             //End Game
@@ -12,9 +17,20 @@ public class Game{
 
     private void setUp(){
         //create three towers
+        
+        for(int i=0;i<3;i++){
+            Tower tower = new Tower();
+            towers.add(tower);
+        }
         //create three discs
+            Disc disc1 = new Disc(10);
+            Disc disc2 = new Disc(20);
+            Disc disc3 = new Disc(30);
+            towers.get(0).addDisc(disc3);
+            towers.get(0).addDisc(disc2);
+            towers.get(0).addDisc(disc1);
+        }
         //add three discs to tower #1
-    }
 
     private void move(int from,int to){
 
@@ -22,6 +38,11 @@ public class Game{
 
     private void show(){
         //display towers
+        for(int i=0; i<towers.size();i++){
+            System.out.println("Tower: " + (i+1));
+            Tower tower = towers.get(i);
+            tower.show();
+        }
     }
 
     private boolean isGameDone(){
--- Visual Studio Code/Tower of Hanoi/Tower.java
+++ Visual Studio Code/Tower of Hanoi/Tower.java
@@ -1,9 +1,18 @@
-public class Tower{
-    public void addDisc(Disc disc){
+import java.util.*;
 
+public class Tower{
+    Deque<Disc> discs = new ArrayDeque<Disc>();
+    public void addDisc(Disc disc){
+        discs.push(disc);
     }
 
     public Disc removeDisc(){
         return null;
+    }
+
+    public void show(){
+        for(Disc disc : discs){
+             System.out.println("          " + disc.getSize() );
+        }
     }
 }
